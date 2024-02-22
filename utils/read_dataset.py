377f07ee85672852e2d691b1230a5525b7204b25
Kurucsai <kurucsaiandris@gmail.com>
2024-02-22 19:18:56 +0100

    demo usage of utils

--- utils/read_dataset.py
+++ utils/read_dataset.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv("Life_Expectancy_Data.csv")
+def read():
+    dataset = pd.read_csv("Life_Expectancy_Data.csv")
 
-raw_data = dataset.values
+    raw_data = dataset.values
 
-cols = range(0, 22)
-X = raw_data[:, cols]
+    cols = range(0, 22)
+    X = raw_data[:, cols]
 
-attributeNames = np.asarray(dataset.columns[cols])
-
-print(attributeNames)
-print(X)
-print(X.shape)
+    attributeNames = np.asarray(dataset.columns[cols])
+    
+    return attributeNames, X
